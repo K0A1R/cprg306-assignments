07bed1b7a1ca2fc324938481bf8ca39fe3f7fe4a
Amrit Reddy <amrit_reddy@me.com>
2024-11-26 15:13:50 -0700

    Add week-2
    Add week-2 (page.js) and student-info component.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Amrit Reddy</p>
+      <Link href="https://github.com/K0A1R/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </section>
+  );
+}
