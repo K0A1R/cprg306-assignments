c7ac7a8f0163811fb54e8e717fa6893d7e8211bd
Amrit Reddy <amrit_reddy@me.com>
2024-11-26 15:23:46 -0700

    Edit page.js (home)
    Add heading and link to week-2.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
 export default function Page() {
-  return <div></div>;
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <nav>
+        <Link href="./week-2">Week 2</Link>
+      </nav>
+    </main>
+  );
 }
