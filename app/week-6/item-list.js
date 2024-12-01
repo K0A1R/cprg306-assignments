75411661bcf7dc52daa199939e41bbdae93a160b
Amrit Reddy <amrit_reddy@me.com>
2024-12-01 15:31:30 -0700

    Edit week-6
    Update item.js, add code from week-3.
    Update page.js, add code from week-3.
    Update item-list.js, import array of items from ItemsJson. Add map function to render individual items.

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,1 +1,12 @@
 import itemsJson from "./items.json";
+import Item from "./item";
+
+export default function ItemList() {
+  return (
+    <div>
+      {itemsJson.map((item) => (
+        <Item id={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
