e50365546a92c9a3b95a5a86b14de0bbf3c6cec8
Amrit Reddy <amrit_reddy@me.com>
2024-12-02 16:07:02 -0700

    Update site wide styling
    Update globals.css, set standards.

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,13 +1,9 @@
 import ItemList from "./item-list";
 export default function Page() {
   return (
-    <body className="bg-slate-950">
-      <main>
-        <h1 className="text-white text-3xl font-bold m-2 pl-3">
-          Shopping List
-        </h1>
-        <ItemList />
-      </main>
-    </body>
+    <main className="bg-slate-950">
+      <h1 className="text-white text-3xl font-bold m-2 pl-3">Shopping List</h1>
+      <ItemList />
+    </main>
   );
 }
