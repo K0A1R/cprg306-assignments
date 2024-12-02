971fdb6b133f2818dac753f1f34722e11aea4e53
Amrit Reddy <amrit_reddy@me.com>
2024-12-01 18:08:15 -0700

    Update week-6 (item-list.js)
    Edit moved buttons above list elements.

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,12 +1,42 @@
+"use client";
 import itemsJson from "./items.json";
 import Item from "./item";
 
+import { useState } from "react";
+
 export default function ItemList() {
+  let items = [...itemsJson];
+  let [sortBy, setSortBy] = useState("name");
+  if (sortBy === "name") {
+    items.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  if (sortBy === "category") {
+    items.sort((a, b) => a.category.localeCompare(b.category));
+  }
   return (
-    <div>
-      {itemsJson.map((item) => (
-        <Item id={item.id} {...item} />
-      ))}
-    </div>
+    <section>
+      <section className="flex gap-5">
+        <p className="text-white font-bold">Sort By:</p>
+        <button
+          className="text-white bg-blue-500 rounded-lg h-8 px-2 hover:bg-blue-600"
+          type="submit"
+          onClick={() => setSortBy("name")}
+        >
+          Name
+        </button>
+        <button
+          className="text-white bg-blue-500 rounded-lg h-8 px-2 hover:bg-blue-600"
+          type="submit"
+          onClick={() => setSortBy("category")}
+        >
+          Category
+        </button>
+      </section>
+      <div>
+        {items.map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
+      </div>
+    </section>
   );
 }
