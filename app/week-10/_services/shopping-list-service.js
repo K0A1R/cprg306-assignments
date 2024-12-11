9c32abf2b3775368da9993c7bbc221dac14bd9b8
Amrit Reddy <amrit_reddy@me.com>
2024-12-10 18:11:06 -0700

    Update week-10
    Add shopping-list-service.js, getItems() gets items from firebase for specific user, addItem adds item to firebase for specific user.
    Update firebase.js, export db.

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,26 @@
+import { db } from "../_utils/firebase.js";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+const getItems = async (userId) => {
+  try {
+    const items = [];
+    const itemsRef = collection(db, `users/${userId}/items`);
+    const itemsSnapshot = await getDocs(itemsRef);
+    itemsSnapshot.forEach((doc) => {
+      items.push({ id: doc.id, ...doc.data() });
+    });
+    return items;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addItem = async (userId, item) => {
+  try {
+    const itemsRef = collection(db, `users/${userId}/items`);
+    const docRef = await addDoc(itemsRef, item);
+    return doc.id;
+  } catch (error) {
+    console.error(error);
+  }
+};
