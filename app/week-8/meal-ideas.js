f22f6d2ac879b4f172febc8e8872fd2e7b420a32
Amrit Reddy <amrit_reddy@me.com>
2024-12-14 14:09:12 -0700

    Update week-8, week-9, week-10
    Update meal-ideas, move fetchMealIdeas() outside the component.

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -1,20 +1,20 @@
 "use client";
 import { useState, useEffect } from "react";
 
+const fetchMealIdeas = async (ingredient) => {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export default function MealIdeas({ ingredient }) {
   const [meals, setMeals] = useState([]);
-
-  const fetchMealIdeas = async (ingredient) => {
-    try {
-      const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const loadMealIdeas = async () => {
     const data = await fetchMealIdeas(ingredient);
@@ -28,18 +28,20 @@ export default function MealIdeas({ ingredient }) {
   return (
     <section className="ml-32 w-96">
       <h1 className="text-white font-bold text-center">Meal Ideas</h1>
-      <div className="bg-slate-900 p-7 h-fit rounded-lg">
-        {meals === null ? (
-          <p className="text-white text-center">
-            No meal ideas found for {ingredient}!
-          </p>
-        ) : (
-          meals.map((meal) => (
-            <p className="text-white" key={meal.idMeal}>
-              {meal.strMeal}
-            </p>
-          ))
-        )}
+      <div>
+        <ul className="bg-slate-900 p-7 h-fit rounded-lg">
+          {meals === null ? (
+            <li className="text-white text-center">
+              No meal ideas found for {ingredient}!
+            </li>
+          ) : (
+            meals.map((meal) => (
+              <li className="text-white" key={meal.idMeal}>
+                {meal.strMeal}
+              </li>
+            ))
+          )}
+        </ul>
       </div>
     </section>
   );
