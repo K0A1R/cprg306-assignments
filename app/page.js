5665f5c8660b9db7cf3330e51a6cdfd9d0b29c02
Amrit Reddy <amrit_reddy@me.com>
2025-01-20 16:42:50 -0700

    Update app\page.js
    Change <div> to <main>

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
+    <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Week 2</Link>
-    </div>
+    </main>
   );
 }
