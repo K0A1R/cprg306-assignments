67c6ba63c13c1cfff1caf196e9a8f4ddb56624cb
Amrit Reddy <amrit_reddy@me.com>
2025-01-25 17:12:48 -0700

    Update week-3
    Update item.js, change structure of item component.

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,11 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <ul className="bg-slate-300 m-3 max-w-md p-2 rounded-md">
-      <li className="font-bold">{name}</li>
-      <li>
-        Buy {quantity} in {category}
+    <ul>
+      <li className="bg-slate-300 m-3 max-w-md p-2 rounded-md">
+        <p className="font-bold">{name}</p>
+        <p>
+          Buy {quantity} in {category}
+        </p>
       </li>
     </ul>
   );
