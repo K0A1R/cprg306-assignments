c14efce5703124d53728c5d78dd63381238bb7dc
Amrit Reddy <amrit_reddy@me.com>
2025-02-06 16:06:33 -0700

    Update week-5
    Update new-item.js, add required attribute to textbox input, capitalize value's of dropdown.

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -26,7 +26,7 @@ export default function NewItem() {
     );
     setQuantity(1);
     setName("");
-    setCategory("produce");
+    setCategory("Produce");
   }
 
   return (
@@ -37,10 +37,12 @@ export default function NewItem() {
       {/* Textbox */}
       <input
         type="text"
+        id="name"
         placeholder="Item Name"
         value={name}
         onChange={(event) => setName(event.target.value)}
         className="rounded-sm hover:shadow-xl focus:shadow-xl focus:outline-none pl-1"
+        required
       ></input>
       {/* Counter + Drop Down Container */}
       <div className="flex items-center justify-center gap-4">
@@ -84,17 +86,17 @@ export default function NewItem() {
           <option value={""} disabled>
             Select Category
           </option>
-          <option value={"produce"}>Produce</option>
-          <option value={"dairy"}>Dairy</option>
-          <option value={"bakery"}>Bakery</option>
-          <option value={"meat"}>Meat</option>
-          <option value={"frozen food"}>Frozen Foods</option>
-          <option value={"canned goods"}>Canned Goods</option>
-          <option value={"dry goods"}>Dry Goods</option>
-          <option value={"beverages"}>Beverages</option>
-          <option value={"snacks"}>Snacks</option>
-          <option value={"household"}>Household</option>
-          <option value={"other"}>Other</option>
+          <option value={"Produce"}>Produce</option>
+          <option value={"Dairy"}>Dairy</option>
+          <option value={"Bakery"}>Bakery</option>
+          <option value={"Meat"}>Meat</option>
+          <option value={"Frozen Food"}>Frozen Foods</option>
+          <option value={"Canned Goods"}>Canned Goods</option>
+          <option value={"Dry Goods"}>Dry Goods</option>
+          <option value={"Beverages"}>Beverages</option>
+          <option value={"Snacks"}>Snacks</option>
+          <option value={"Household"}>Household</option>
+          <option value={"Other"}>Other</option>
         </select>
       </div>
       <button
