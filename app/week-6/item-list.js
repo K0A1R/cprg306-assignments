1f39e5ea8127dd120dc96c1d9c67ac0ed66fe79e
Amrit Reddy <amrit_reddy@me.com>
2025-02-16 15:00:43 -0700

    Update week-6
    Update item-list.js, add state (sortBy), check if sort by name or category. Sort list based on either name or category. Add Name and Category buttons, onClick sorts based on Name or Category.

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,10 +1,41 @@
+"use client";
 import itemsData from "./items.json";
 import Item from "./item";
 
+import { useState } from "react";
+
 export default function ItemList() {
+  const [sortBy, setSortBy] = useState("name");
+  const items = [...itemsData];
+
+  if (sortBy === "name") {
+    items.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortBy === "category") {
+    items.sort((a, b) => a.category.localeCompare(b.category));
+  }
+
   return (
     <div>
-      {itemsData.map((item) => (
+      <div className="flex items-center gap-2 ml-4">
+        <p>Sort By:</p>
+        <button
+          className={`rounded-md p-1 w-24 ${
+            sortBy === "name" ? "bg-green-700 text-white" : "bg-green-400"
+          }`}
+          onClick={() => setSortBy("name")}
+        >
+          Name
+        </button>
+        <button
+          className={`rounded-md p-1 w-24 ${
+            sortBy === "category" ? "bg-green-700 text-white" : "bg-green-400"
+          }`}
+          onClick={() => setSortBy("category")}
+        >
+          Category
+        </button>
+      </div>
+      {items.map((item) => (
         <Item
           key={item.id}
           name={item.name}
