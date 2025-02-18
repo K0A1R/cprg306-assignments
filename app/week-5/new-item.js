da4ed898e794ea00000281f9c26c8abc40c8cd6e
Amrit Reddy <amrit_reddy@me.com>
2025-02-18 14:53:37 -0700

    Update week-5
    Update new-item.js, minor change useState initial value "produce" to "Produce".

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function NewItem() {
   const [quantity, setQuantity] = useState(1);
   const [name, setName] = useState("");
-  const [category, setCategory] = useState("produce");
+  const [category, setCategory] = useState("Produce");
 
   function increment() {
     if (quantity < 20) {
