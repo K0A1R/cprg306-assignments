e1f3abff48e2bbd5e2fd5e58bb926b0fbd047446
Amrit Reddy <amrit_reddy@me.com>
2025-02-23 15:11:36 -0700

    Update page.js (home)
    Update page.js, remove individual <Link>. Use array of weeks to create <Link>.

--- app/page.js
+++ app/page.js
@@ -1,42 +1,23 @@
 import Link from "next/link";
 
 export default function Page() {
+  const weeks = [2, 3, 4, 5, 6];
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="font-bold text-xl">
         CPRG 306: Web Development 2 - Assignments
       </h1>
       <nav className="flex flex-col">
-        <Link
-          className="hover:underline hover:text-blue-600 font-semibold"
-          href="/week-2"
-        >
-          Week 2 Assignment
-        </Link>
-        <Link
-          className="hover:underline hover:text-blue-600 font-semibold"
-          href="/week-3"
-        >
-          Week 3 Assignment
-        </Link>
-        <Link
-          className="hover:underline hover:text-blue-600 font-semibold"
-          href="/week-4"
-        >
-          Week 4 Assignment
-        </Link>
-        <Link
-          className="hover:underline hover:text-blue-600 font-semibold"
-          href="/week-5"
-        >
-          Week 5 Assignment
-        </Link>
-        <Link
-          className="hover:underline hover:text-blue-600 font-semibold"
-          href="/week-6"
-        >
-          Week 6 Assignment
-        </Link>
+        <ul>
+          {weeks.map((week) => (
+            <li
+              key={week}
+              className="hover:underline hover:text-blue-600 font-semibold"
+            >
+              <Link href={`/week-${week}`}>Week {week} Assignment</Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </main>
   );
